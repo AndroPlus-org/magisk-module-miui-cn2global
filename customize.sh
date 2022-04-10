37e4ef9eb2462d89c06855be5c91ec0af89b9ec5
AndroPlus-org <hosigumayuugi@gmail.com>
2022-04-10 19:56:24 +0900

    Google電話アプリを入れていない時の受話対策

--- customize.sh
+++ customize.sh
@@ -17,6 +17,18 @@ elif [ -f "/system/etc/permissions/services.cn.google.xml" ]; then
     PERMISSION_PATH="${MODPATH}/system/etc/permissions"
 	remove_limitation "${PERMISSION_PATH}"
 else
-    ui_print "services.cn.google.xml Not found!"
+    ui_print "services.cn.google.xml はありませんでした"
 fi
 
+DEF_DIALER=`pm list packages|grep com.google.android.dialer`
+if [ -n "$DEF_DIALER" ]; then
+    cp -a ${MODPATH}/cn2g-optional/GmsConfigOverlayComms.apk ${MODPATH}/system/product/overlay
+    ui_print ""
+    ui_print "******⚠注意⚠******"
+    ui_print "Google 電話アプリを必ずデフォルトの電話アプリに設定してください。"
+    ui_print "設定していないと受話できなくなります。"
+    ui_print "******⚠注意⚠******"
+    ui_print ""
+fi
+
+rm -rf ${MODPATH}/cn2g-optional
