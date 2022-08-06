3101627fd19a4e416ef0813bae8ade817e7dea06
AndroPlus-org <hosigumayuugi@gmail.com>
2022-08-06 14:49:51 +0900

    Add more lang

--- customize.sh
+++ customize.sh
@@ -17,7 +17,7 @@ elif [ -f "/system/etc/permissions/services.cn.google.xml" ]; then
     PERMISSION_PATH="${MODPATH}/system/etc/permissions"
 	remove_limitation "${PERMISSION_PATH}"
 else
-    ui_print "services.cn.google.xml はありませんでした"
+    ui_print "services.cn.google.xm not found, skipped"
 fi
 
 DEF_DIALER=`pm list packages|grep com.google.android.dialer`
@@ -25,9 +25,17 @@ if [ -n "$DEF_DIALER" ]; then
     cp -a ${MODPATH}/cn2g-optional/GmsConfigOverlayComms.apk ${MODPATH}/system/product/overlay
     ui_print ""
     ui_print "******⚠注意⚠******"
-    ui_print "Google 電話アプリを必ずデフォルトの電話アプリに設定してください。"
+    ui_print "Google 電話アプリを必ずデフォルトの電話アプリに"
+    ui_print "設定して、権限や通知、自動起動を設定してください。"
     ui_print "設定していないと受話できなくなります。"
     ui_print "******⚠注意⚠******"
+    ui_print ""
+    ui_print ""
+    ui_print "******⚠ WARNING ⚠******"
+    ui_print "Please set Google Phone app as default-"
+    ui_print "phone app and setup permissions / autostart."
+    ui_print "If you don't, you can't receive a call."
+    ui_print "******⚠ WARNING ⚠******"
     ui_print ""
 fi
 
