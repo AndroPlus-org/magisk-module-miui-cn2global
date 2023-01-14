7ea24c2f634706de9f5d776162af160163ed2061
AndroPlus-org <hosigumayuugi@gmail.com>
2023-01-14 21:52:14 +0900

    MIUI 14対応

--- customize.sh
+++ customize.sh
@@ -4,18 +4,21 @@ REPLACE="
 remove_limitation() {
 mkdir -p "$1"
 
-cat >"$1/services.cn.google.xml" <<EOF
+cat >"$1/$2" <<EOF
 <?xml version="1.0" encoding="utf-8"?>
 <!-- This is the standard set of features for devices that support the CN GMSCore. -->
 EOF
 }
 
-if [ -f "/system/product/etc/permissions/services.cn.google.xml" ]; then
+if [ -f "/system/product/etc/permissions/cn.google.services.xml" ]; then
     PERMISSION_PATH="${MODPATH}/system/product/etc/permissions"
-	remove_limitation "${PERMISSION_PATH}"
+	remove_limitation "${PERMISSION_PATH}" "cn.google.services.xml"
+elif [ -f "/system/product/etc/permissions/services.cn.google.xml" ]; then
+    PERMISSION_PATH="${MODPATH}/system/product/etc/permissions"
+	remove_limitation "${PERMISSION_PATH}" "services.cn.google.xml"
 elif [ -f "/system/etc/permissions/services.cn.google.xml" ]; then
     PERMISSION_PATH="${MODPATH}/system/etc/permissions"
-	remove_limitation "${PERMISSION_PATH}"
+	remove_limitation "${PERMISSION_PATH}" "services.cn.google.xml"
 else
     ui_print "services.cn.google.xm not found, skipped"
 fi
