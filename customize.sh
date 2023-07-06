9ad6dc4634f9224ccd76c3da2a3ba40796301a81
AndroPlus <mail@andro.plus>
2023-07-06 09:40:47 +0900

    Temporarily remove KernelSU support

--- customize.sh
+++ customize.sh
@@ -3,19 +3,10 @@ REPLACE="
 
 # Taken from unlock-cn-gms
 # Credit: Howard20181, yujincheng08 https://github.com/yujincheng08/unlock-cn-gms
-PERMISSIONS_PATH=/etc/permissions
-SYSTEM_PATH=/system
-SYSTEM_EXT_PATH=/system_ext
-PRODUCT_PATH=/product
-VENDOR_PATH=/vendor
+VENDOR_PATH=$SYSTEM_PATH/vendor
 OPLUS_BIGBALL_PATH=/my_bigball
 OPLUS_BIGBALL_VENDOR_PATH=/mnt/vendor$OPLUS_BIGBALL_PATH
-if [ -z "$KSU" ]; then
-    ROOT_LIST=""$SYSTEM_PATH$PERMISSIONS_PATH" "$PRODUCT_PATH$PERMISSIONS_PATH" "$VENDOR_PATH$PERMISSIONS_PATH" "$SYSTEM_EXT_PATH$PERMISSIONS_PATH" "$OPLUS_BIGBALL_PATH$PERMISSIONS_PATH" "$OPLUS_BIGBALL_VENDOR_PATH$PERMISSIONS_PATH""
-    REMOVE=""
-else # Magisk
-    ROOT_LIST=""$SYSTEM_PATH$PERMISSIONS_PATH" "$SYSTEM_PATH$PRODUCT_PATH$PERMISSIONS_PATH" "$SYSTEM_PATH$VENDOR_PATH$PERMISSIONS_PATH" "$SYSTEM_PATH$SYSTEM_EXT_PATH$PERMISSIONS_PATH" "$OPLUS_BIGBALL_PATH$PERMISSIONS_PATH" "$OPLUS_BIGBALL_VENDOR_PATH$PERMISSIONS_PATH""
-fi
+ROOT_LIST=""$SYSTEM_PATH$PERMISSIONS_PATH" "$PRODUCT_PATH$PERMISSIONS_PATH" "$VENDOR_PATH$PERMISSIONS_PATH" "$SYSTEM_EXT_PATH$PERMISSIONS_PATH" "$OPLUS_BIGBALL_PATH$PERMISSIONS_PATH" "$OPLUS_BIGBALL_VENDOR_PATH$PERMISSIONS_PATH""
 FILE_LIST="services.cn.google.xml cn.google.services.xml oplus_google_cn_gms_features.xml"
 for ROOT in $ROOT_LIST; do
     for FILE in $FILE_LIST; do
@@ -23,24 +14,20 @@ for ROOT in $ROOT_LIST; do
             PERMISSION_PATH="$MODPATH$ROOT"
             FILE_NAME=$FILE
             ui_print "- PATH $ROOT/$FILE_NAME"
-            if [ -z "$KSU" ] && { [ "$ROOT" != "$OPLUS_BIGBALL_PATH$PERMISSIONS_PATH" ] || [ "$ROOT" != "$OPLUS_BIGBALL_VENDOR_PATH$PERMISSIONS_PATH" ]; }; then
-                REMOVE="$REMOVE $ROOT/$FILE_NAME"
-            else
-                mkdir -p "$PERMISSION_PATH"
-                cat >"$PERMISSION_PATH/$FILE_NAME" <<EOF
+            mkdir -p "$PERMISSION_PATH"
+            cat >"$PERMISSION_PATH/$FILE_NAME" <<EOF
 <?xml version="1.0" encoding="utf-8"?>
 <!-- This is the standard set of features for devices that support the CN GMSCore. -->
 EOF
-                [ "$ROOT" = "$OPLUS_BIGBALL_PATH$PERMISSIONS_PATH" ] || [ "$ROOT" = "$OPLUS_BIGBALL_VENDOR_PATH$PERMISSIONS_PATH" ] && {
-                    if [ ! -f "$MODPATH/post-fs-data.sh" ]; then
-                        cat >"$MODPATH/post-fs-data.sh" <<EOF
+            [ "$ROOT" = "$OPLUS_BIGBALL_PATH$PERMISSIONS_PATH" ] || [ "$ROOT" = "$OPLUS_BIGBALL_VENDOR_PATH$PERMISSIONS_PATH" ]  && {
+                if [ ! -f "$MODPATH/post-fs-data.sh" ]; then
+                    cat >"$MODPATH/post-fs-data.sh" <<EOF
 #!/system/bin/sh
 MODDIR=\${0%/*}
 EOF
-                    fi
-                    echo "mount -o ro,bind \$MODDIR$ROOT/$FILE_NAME $ROOT/$FILE_NAME" >>"$MODPATH/post-fs-data.sh"
-                }
-            fi # End else
+                fi
+                echo "mount -o ro,bind \$MODDIR$ROOT/$FILE_NAME $ROOT/$FILE_NAME" >> "$MODPATH/post-fs-data.sh"
+            }
         fi
     done
 done
